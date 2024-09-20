46f0a44648ae6d6cd9e1a1c86175471927ae8dff
Opinaka <52041843+opinaka@users.noreply.github.com>
2024-09-20 20:41:20 +0200

    Add files via upload

---
+++ src/Router.js
@@ -0,0 +1,67 @@
+import * as React from "react";
+import { Switch, Route } from "react-router-dom";
+import Root from "./pages/root";
+import Elasticsearch from "./pages/elasticsearch";
+import ElasticsearchWithAnalytics from "./pages/elasticsearch-with-analytics";
+import Engines from "./pages/engines";
+import AppSearch from "./pages/app-search";
+import SiteSearch from "./pages/site-search";
+import WorkplaceSearch from "./pages/workplace-search";
+import SearchAsYouType from "./pages/search-as-you-type";
+import CustomizingStylesAndHtml from "./pages/customizing-styles-and-html";
+import SearchBarInHeaderIndex from "./pages/search-bar-in-header/index";
+import SearchBarInHeaderSearch from "./pages/search-bar-in-header/search";
+import EcommerceHome from "./pages/ecommerce";
+import EcommerceSearch from "./pages/ecommerce/Search";
+import EcommerceCategory from "./pages/ecommerce/Category";
+import ListingPage from "./pages/ecommerce/ListingPage";
+
+export default function Router() {
+  return (
+    <div className="App">
+      <Switch>
+        <Route exact path="/" component={Root} />
+
+        {/* Connectors */}
+        <Route exact path="/elasticsearch" component={Elasticsearch} />
+        <Route exact path="/app-search" component={AppSearch} />
+        <Route exact path="/site-search" component={SiteSearch} />
+        <Route exact path="/workplace-search" component={WorkplaceSearch} />
+        <Route exact path="/engines" component={Engines} />
+
+        {/* Examples */}
+        <Route exact path="/search-as-you-type" component={SearchAsYouType} />
+        <Route
+          exact
+          path="/customizing-styles-and-html"
+          component={CustomizingStylesAndHtml}
+        />
+        <Route
+          exact
+          path="/search-bar-in-header"
+          component={SearchBarInHeaderIndex}
+        />
+        <Route
+          exact
+          path="/search-bar-in-header/search"
+          component={SearchBarInHeaderSearch}
+        />
+        <Route
+          exact
+          path="/elasticsearch-with-analytics"
+          component={ElasticsearchWithAnalytics}
+        />
+
+        {/* Use cases */}
+        <Route exact path="/ecommerce/" component={EcommerceHome} />
+        <Route exact path="/ecommerce/search" component={EcommerceSearch} />
+        <Route exact path="/ecommerce/all" component={ListingPage} />
+        <Route
+          exact
+          path="/ecommerce/category/:category"
+          component={EcommerceCategory}
+        />
+      </Switch>
+    </div>
+  );
+}
